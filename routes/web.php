cfb3786c34729e676a9704db8e7e8727a0f60fe0
vaniararaandini03 <vania24ti@mahasiswa.pcr.ac.id>
2025-10-27 18:37:00 +0700

    P7

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,55 @@
+<?php
+
+use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\MatakuliahController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PelangganController;
+
 Route::get('/pcr', function () {
     return 'Selamat Datang di Website Kampus PCR!';
 });
+
+Route::get('/mahasiswa', function () {
+    return 'Halo Mahasiswa';
+});
+
+Route::get('/nama/{Yolanda}', function ($param1) {
+    return 'Nama Saya: '.$param1;
+});
+
+Route::get('/nim/{pram1?}', function ($param1 = '') {
+    return 'NIM Saya: '.$param1;
+});
+
+Route::get('/mahasiswa/{Yolanda}', [MahasiswaController::class, 'show']);
+
+Route::get('/about', function (){
+    return view('halaman-about');
+});
+
+Route::get('/matakuliahshow', [MatakuliahController::class, 'show']);
+Route::get('/matakuliahindex', [MatakuliahController::class, 'index']);
+Route::get('/matakuliahcreate', [MatakuliahController::class, 'create']);
+Route::get('/matakuliahstore', [MatakuliahController::class, 'store']);
+Route::get('/matakuliahedit', [MatakuliahController::class, 'edit']);
+Route::get('/matakuliahupdate', [MatakuliahController::class, 'update']);
+Route::get('/matakuliahdestroy', [MatakuliahController::class, 'destroy']);
+
+Route::get('/matakuliah/{param1?}', [MahasiswaController::class, 'show']);
+Route::get('/home', [HomeController::class, 'index'])-> name('home');
+
+Route::post('question/store', [QuestionController::class, 'store'])
+            -> name('question.store');
+
+Route::get('dashboard', [DashboardController::class, 'index'])
+            -> name('dashboard');
+
+Route::resource('pelanggan', PelangganController::class);
+
+Route::get('/pelanggan/{id}/edit', [PelangganController::class, 'edit'])->name('pelanggan.edit');
+
+Route::put('/pelanggan/{id}', [PelangganController::class, 'update'])->name('pelanggan.update');
+
