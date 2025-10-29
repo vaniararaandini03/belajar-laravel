e7477d558217ac0d2107be0ae579d9ebad463754
vaniararaandini03 <vania24ti@mahasiswa.pcr.ac.id>
2025-10-29 20:07:21 +0700

    p

---
+++ app/Http/Controllers/QuestionController.php
@@ -0,0 +1,78 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class QuestionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nama'       => 'required|min:5|max:50',
+            'email'      => 'required|email',
+            'pertanyaan' => 'required|min:10|max:300'
+        ], [
+            'nama.required' => "Nama Tidah boleh kosong",
+            'email.required' => "Email Tidak valid"
+        ]);
+
+        $nama       = $request->input('nama');
+        $email      = $request->input('email');
+        $pertanyaan = $request->input('pertanyaan');
+
+        //return view('home-question-respon', compact('nama', 'email', 'pertanyaan'));
+        return redirect()->back()->with('info', "Terimakasih $nama! Pertanyaan Kamu berhasil terkirim, dan akan segera kami tanggapi dan balas melalui $email");
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $data['dataUser'] = User::all();
+		return view('admin.user.index',$data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('admin.user.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name'  => 'required|string|max:100',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:8|confirmed'
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+		return redirect()->route('user.index')->with('success','Data Berhasil Ditambahkan!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $data['dataUser'] = User::findOrFail($id);
+        return view('admin.user.edit', $data);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $data['name'] = $request->name;
+        $data['email'] = $request->email;
+        $data['password'] = Hash::make($request->password);
+
+        User::where('id', $id)->update($data);
+        return redirect()->route('user.index')->with('success', 'Perubahan Data Berhasil!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $user = User::findOrFail($id);
+
+        $user->delete();
+        return redirect()->route('user.index')->with('success', 'Data Berhasil Dihapus!');
+    }
+}
