36b0de58f7de32a0a14c46f51e235d3a5712042e
vaniararaandini03 <vania24ti@mahasiswa.pcr.ac.id>
2025-11-20 17:44:02 +0700

    .

--- database/seeders/CreateFirstUser.php
+++ database/seeders/CreateFirstUser.php
@@ -2,15 +2,8 @@
 
 namespace Database\Seeders;
 
-<<<<<<< HEAD
-use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
-=======
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
->>>>>>> 8e92dacc93ec66d56a73cc99ba20d5a12de4db4d
 
 class CreateFirstUser extends Seeder
 {
@@ -19,14 +12,6 @@ class CreateFirstUser extends Seeder
      */
     public function run(): void
     {
-<<<<<<< HEAD
-        User::create([
-            'name' => 'Admin',
-            'email' => 'gator@pcr.ac.id',
-            'password' => Hash::make('gatotkaca')
-        ]);
-=======
         //
->>>>>>> 8e92dacc93ec66d56a73cc99ba20d5a12de4db4d
     }
 }
