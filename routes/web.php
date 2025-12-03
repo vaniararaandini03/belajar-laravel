f3b94086c835ae9e03be50bdc0c33df98d5c5a5f
vaniararaandini03 <vania24ti@mahasiswa.pcr.ac.id>
2025-12-03 12:25:55 +0700

    .

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PelangganController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MultipleuploadsController;
 
 Route::get('/pcr', function () {
     return 'Selamat Datang di Website Kampus PCR!';
@@ -18,7 +19,7 @@ Route::get('/mahasiswa', function () {
     return 'Halo Mahasiswa';
 });
 
-Route::get('/nama/{Yolanda}', function ($param1) {
+Route::get('/nama/{Vania}', function ($param1) {
     return 'Nama Saya: '.$param1;
 });
 
@@ -26,7 +27,7 @@ Route::get('/nim/{pram1?}', function ($param1 = '') {
     return 'NIM Saya: '.$param1;
 });
 
-Route::get('/mahasiswa/{Yolanda}', [MahasiswaController::class, 'show']);
+Route::get('/mahasiswa/{Vania}', [MahasiswaController::class, 'show']);
 
 Route::get('/about', function (){
     return view('halaman-about');
@@ -40,7 +41,8 @@ Route::get('/matakuliahedit', [MatakuliahController::class, 'edit']);
 Route::get('/matakuliahupdate', [MatakuliahController::class, 'update']);
 Route::get('/matakuliahdestroy', [MatakuliahController::class, 'destroy']);
 
-Route::get('/matakuliah/{param1?}', [MahasiswaController::class, 'show']);
+Route::get('/matakuliah/{param1?}', [MatakuliahController::class, 'show']);
+
 Route::get('/home', [HomeController::class, 'index'])-> name('home');
 
 Route::post('question/store', [QuestionController::class, 'store'])
@@ -57,10 +59,9 @@ Route::resource('user', UserController::class);
 Route::get('/user/{id}/edit', [UserController::class, 'edit'])->name('user.edit');
 Route::put('/user/{id}', [UserController::class, 'update'])->name('user.update');
 
-
-Route::get('/auth', function () {
-    return view('home');
-
-});
 Route::get('/auth', [AuthController::class, 'index']);
 Route::post('/auth/login', [AuthController::class, 'login']);
+
+Route::get('/multipleuploads', 'MultipleuploadsController@index')->name('uploads');
+Route::post('/save','MultipleuploadsController@store')->name('uploads.store');
+
